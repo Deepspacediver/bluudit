473c39c3c55c5d8a38c81d107c58cbaa5ea8860c
deepspacediver <luka.koscik@gmail.com>
2023-03-06 18:28:13 +0100

    preview of the signup

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,8 @@
 import React from "react";
+import SignUpModal from "./components/SignUpModal";
 
-const App = () =>  {
-    return <h1>Hello World</h1>;
-}
+const App = () => {
+  return <SignUpModal />;
+};
 
 export default App;
